ac407bb2b722d047c408ed6f1f8eb8f09c9861ee
Nir Channes <nirchannes@Nirs-MacBook-Pro.local>
2018-03-05 21:42:51 +0200

    Dynamic demo page! :)

--- demo/demo.js
+++ demo/demo.js
@@ -1,18 +1,2 @@
 
 var demoApp = angular.module("demoApp", []);
-
-// app.config(function($routeProvider) {
-//     $routeProvider
-//     .when("/:model_id", {
-//         templateUrl : "main.htm"
-//     })
-//     .when("/red", {
-//         templateUrl : "red.htm"
-//     })
-//     .when("/green", {
-//         templateUrl : "green.htm"
-//     })
-//     .when("/blue", {
-//         templateUrl : "blue.htm"
-//     });
-// });
--- demo/demoCtrl.js
+++ demo/demoCtrl.js
@@ -1,13 +1,23 @@
 
-demoApp.controller('demoCtrl', function($scope, $sce) {
+demoApp.controller('demoCtrl', function($scope, $sce, $location, $window) {
     var models = {
-        "": {
+        "office-chair": {
             name: "Office Chair",
             claraId: "0f0e7e13-c57b-471d-b3b6-4091bd051ab0"
+        },
+        "tanning-bed": {
+            name: "Tanning Bed",
+            claraId: "c9fd9abe-67cc-43c7-96e1-d8ecf59c68af"
         }
     }
 
-    $scope.modelName = "Office Chair";
-    $scope.claraEmbedUrl = "https://clara.io/player/v2/0f0e7e13-c57b-471d-b3b6-4091bd051ab0?tools=hide";
-    $scope.claraEmbedUrl = $sce.trustAsResourceUrl($scope.claraEmbedUrl);
+    var modelId = $location.path().substring(1, $location.path().length);
+    var model = models[modelId];
+
+    if (!model) {
+        $window.location.href = "http://www.polyview3d.com";
+    } else {
+        $scope.modelName = model.name;
+        $scope.claraEmbedUrl = $sce.trustAsResourceUrl("https://clara.io/player/v2/" + model.claraId + "?tools=hide");    
+    }
 });

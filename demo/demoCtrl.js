5583f7644d205a8ec8406c113617d826133c299f
Nir Channes <nirchannes@Nirs-MacBook-Pro.local>
2018-03-11 17:43:11 +0200

    kara armchair added to demos

--- demo/demoCtrl.js
+++ demo/demoCtrl.js
@@ -8,13 +8,18 @@ demoApp.controller('demoCtrl', function($scope, $sce, $location, $window) {
         "tanning-bed": {
             name: "Tanning Bed",
             claraId: "c9fd9abe-67cc-43c7-96e1-d8ecf59c68af"
+        },
+        "kara-armchair": {
+            name: "Kara Armchair",
+            claraId: "597cbc9c-7ae6-47a0-bbc7-440691cc8987"            
         }
     }
 
+    // Getting the model id from the URL and checking it exists
     var modelId = $location.path().substring(1, $location.path().length);
     var model = models[modelId];
-
     if (!model) {
+        // Sending the user back to the landing page if the model url doesn't exist
         $window.location.href = "http://www.polyview3d.com";
     } else {
         $scope.modelName = model.name;

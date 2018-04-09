8ee23afe74c3342bc2d97e9c8ef16e07a5af4ba4
Nir Channes <nirchannes@Nirs-MacBook-Pro.local>
2018-04-09 22:01:18 +0300

    Gallery + Item working

--- demos/app/item/demoItemCtrl.js
+++ demos/app/item/demoItemCtrl.js
@@ -1,8 +1,9 @@
 
-app.controller('demoItemCtrl', function($scope, modelService, $routeParams) {
+app.controller('demoItemCtrl', function($scope, modelService, $routeParams, $sce) {
 
     modelService.load().then(function() {
         $scope.model = modelService.models[$routeParams.modelKey];
+        $scope.model.claraEmbedId = $sce.trustAsResourceUrl("https://clara.io/player/v2/" + $scope.model.claraId + "?tools=hide");
     });
     
 });

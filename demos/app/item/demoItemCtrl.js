42bf642b5546c5f897b539a3d7471f0d0ec67f5f
Nir Channes <nirchannes@Nirs-MacBook-Pro.local>
2018-04-16 11:56:53 +0300

    in case of model key not found going to gallery

--- demos/app/item/demoItemCtrl.js
+++ demos/app/item/demoItemCtrl.js
@@ -1,9 +1,15 @@
 
-app.controller('demoItemCtrl', function($scope, modelService, $routeParams, $sce) {
+app.controller('demoItemCtrl', function($scope, modelService, $routeParams, $sce, $location) {
 
     modelService.load().then(function() {
         $scope.model = modelService.models[$routeParams.modelKey];
-        $scope.model.claraEmbedId = $sce.trustAsResourceUrl("https://clara.io/player/v2/" + $scope.model.claraId + "?tools=hide");
+
+        if ($scope.model) {
+            $scope.model.claraEmbedId = $sce.trustAsResourceUrl("https://clara.io/player/v2/" + $scope.model.claraId + "?tools=hide");
+        } else {
+            // no such model, going back to demos page
+            $location.path('/');
+        }
     });
     
 });

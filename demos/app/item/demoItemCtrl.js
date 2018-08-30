3103bf873c69de14ae0fea17bed1a598b992915c
nirch <nirh2@yahoo.com>
2018-08-30 17:23:06 +0300

    Color variations feature added :)

--- demos/app/item/demoItemCtrl.js
+++ demos/app/item/demoItemCtrl.js
@@ -5,11 +5,18 @@ app.controller('demoItemCtrl', function($scope, modelService, $routeParams, $sce
         $scope.model = modelService.models[$routeParams.modelKey];
 
         if ($scope.model) {
-            $scope.model.claraEmbedId = $sce.trustAsResourceUrl("https://clara.io/player/v2/" + $scope.model.claraId + "?tools=hide");
+            if (!$scope.model.colorVariations) {
+                $scope.model.claraEmbedId = $sce.trustAsResourceUrl("https://clara.io/player/v2/" + $scope.model.claraId + "?tools=hide");
+            } else {
+                $scope.model.claraEmbedId = $sce.trustAsResourceUrl("https://clara.io/player/v2/" + $scope.model.colorVariations[0].claraId + "?tools=hide");
+            }
         } else {
             // no such model, going back to demos page
             $location.path('/');
         }
     });
-    
+
+    $scope.updateModel = function(colorVariation) {
+        $scope.model.claraEmbedId = $sce.trustAsResourceUrl("https://clara.io/player/v2/" + colorVariation.claraId + "?tools=hide");
+    }    
 });

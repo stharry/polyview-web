2db9db87f1cee3f4422c6d687aa317ac7e51230d
nirch <nirh2@yahoo.com>
2018-09-28 09:43:33 +0300

    House model: changed demo name + unlisted + analytics

--- demos/app/gallery/demoGalleryCtrl.js
+++ demos/app/gallery/demoGalleryCtrl.js
@@ -9,6 +9,15 @@ app.controller('demoGalleryCtrl', function($scope, modelService, $location) {
                 $scope.models[modelKey] = modelService.models[modelKey];
             }
         }
+
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', 'UA-115185862-1', {
+            'page_title' : "Polyview Demo Gallery",
+            'page_path': "/demos"
+        });
+
     });
     
     $scope.openModel = function(modelKey) {
--- demos/app/item/demoItemCtrl.js
+++ demos/app/item/demoItemCtrl.js
@@ -10,6 +10,15 @@ app.controller('demoItemCtrl', function($scope, modelService, $routeParams, $sce
             } else {
                 $scope.model.claraEmbedId = $sce.trustAsResourceUrl("https://clara.io/player/v2/" + $scope.model.colorVariations[0].claraId + "?tools=hide");
             }
+
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'UA-115185862-1', {
+                'page_title' : "Polyview Demo - " + $scope.model.name,
+                'page_path': "/demos/#!" + $location.path()
+            });
+        
         } else {
             // no such model, going back to demos page
             $location.path('/');
